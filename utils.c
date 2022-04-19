de7d5311dd0e18b071143bf769443f628b1a9ee3
timothe-bramas <90620809+timothe-bramas@users.noreply.github.com>
2022-04-19 14:55:18 +0200

    ajout des utils.c et utils.h

---
+++ utils.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+double moyenne(double*data, int length){
+    double s=0;
+    for(int i=0;i<length;i++){s+=data[i];}
+    return s/length;
+}
+
+
+
+
+double* reduction_vect(double*data, int height, int weight){
+    double moy;
+    double var;
+    double*ligne;
+    double* tab=malloc(2*height*sizeof(double));
+    for(int i=0; i<height; i++){
+        for(int j=0;j<weight;j++){ligne[j]=data[i*weight+j];}   //Récupération de la ligne
+        moy=moyenne(ligne, weight);
+        tab[2*i]=moy;
+        for(int j=0;j<weight;j++){ligne[j]=(ligne[j]-moy)*(ligne[j]-moy);}
+        var=moyenne(ligne, weight);
+        tab[2*i+1]=var;
+        } 
+    return tab;    
+    }

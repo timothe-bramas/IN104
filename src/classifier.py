0c223537d281a6ceb04d5b1176d878e78af1b2e1
timothe-bramas <timothe.bramas@gmail.com>
2022-05-23 10:26:00 +0200

    new matrix, script is working

--- src/classifier.py
+++ src/classifier.py
@@ -20,7 +20,7 @@ y = batch_audio[:, 0]
 
 X_train, X_test, y_train, y_test = train_test_split(features, y, test_size=0.2, random_state=0)
 
-model = LinearSVC(C=0.1, max_iter=100, tol=1e-4) 
+model = LinearSVC(C=0.1, max_iter=1000, tol=1e-5) 
 
 # On peut modifier les paramètres C, max_iter et tol, pour augmenter la performance du modèle.
 
--- src/main.c
+++ src/main.c
@@ -21,35 +21,33 @@ if (argc < 2) {
 int nRow;
 int nCol;
 FILE* f=fopen(argv[1], "wb");
-double* magn=malloc(257*sizeof(double));
-double* mean=malloc(257*sizeof(double));
-double* std;
+double* magn;
 double*magncorr;
 char* genres[] = {"../../genres/blues/blues.00000.wav","../../genres/classical/classical.00000.wav","../../genres/country/country.00000.wav","../../genres/disco/disco.00000.wav","../../genres/hiphop/hiphop.00000.wav","../../genres/jazz/jazz.00000.wav","../../genres/metal/metal.00000.wav","../../genres/pop/pop.00000.wav","../../genres/reggae/reggae.00000.wav","../../genres/rock/rock.00000.wav"};
 char temp[256];
 
 //magn = magnitude("../../genres/blues/blues.00000.wav", &height, &width);
 //printf("%d %d\n",height,width);}
+magn=magnitude("../../genres/blues/blues.00000.wav", &nCol, &nRow);
+double* mean=malloc(nRow*sizeof(double));
+double* std=malloc(nRow*sizeof(double));
 
 
 
-
-for (int i = 0 ; i<5 ; i++) { // Boucle sur les 10 genres
+for (int i = 8 ; i < 10 ; i++) { // Boucle sur les 10 genres
 int len_temp=strlen(genres[i]);
 strcpy(temp,genres[i]); // temp contiendra le nom de la musique numéro l du genre i
 
 
-for (int l=5 ; l<100 ; l++) { // Boucle sur le numéro de la musique
+for (int l=0 ; l<100 ; l++) { // Boucle sur le numéro de la musique
     char c1 = l%10 + '0'; // Chiffre des unités
     char c2 = (l%100 - l%10)/10 + '0'; // Chiffre des dizaines
     temp[len_temp-1-4]=c1;
     temp[len_temp-2-4]=c2;
     magn = magnitude(temp, &nCol, &nRow);
     reduction_vect2(magn, nCol, nRow, mean, std);
-    double mean[nRow];
-    double std[nRow];
-    fprintf(f, "%d;", i);
-    for(int k=0; k<nRow*2; k++){fprintf(f, "%.2f, %.2f, ", mean[0], std[0]); }
+    fprintf(f, "%d", i);
+    for(int k=0; k<nRow; k++){fprintf(f, ";%.2f;%.2f", mean[k], std[k]); }
     fprintf(f, "\n");
   }   
 }
